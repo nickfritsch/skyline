4717a934c531491c5b22cb1e524fa8c311e3b874
nickfritsch <nfritsch@gmail.com>
2019-02-21 09:52:14 -0800

    Update CreateSkylineRole.ps1

--- CreateSkylineRole.ps1
+++ CreateSkylineRole.ps1
@@ -8,6 +8,17 @@
     Script to create vCenter Server role within vCenter Server with all privileges needed for Skyline Collector and Skyline Log Assist to function properly.
     
 #>
+$vCenterServer = "vCenter_Server_FQDN/IP_Address"
+
+$RoleExists = Get-VIRole -Server $vCenterServer -Role Skyline
+
+If ($RoleExists) {
+    Write "Skyline Role Already Exists"
+    Exit
+}
+Else {
+    Write "Skyline Role Will Be Created"
+
 New-VIRole -Name Skyline -Privilege (Get-VIPrivilege -Role Readonly)
 Set-VIRole -Role Skyline -AddPrivilege (Get-VIPrivilege -id Global.Licenses)
 Set-VIRole -Role Skyline -AddPrivilege (Get-VIPrivilege -id Global.Settings)
